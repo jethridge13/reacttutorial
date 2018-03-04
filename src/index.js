37a617c63d155fae1c4155f39a5ddae4a11ce50b
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-03-03 21:28:55 -0500

    Challenge #3: Refactor to use loops instead

--- src/index.js
+++ src/index.js
@@ -35,32 +35,24 @@ class Board extends React.Component {
 
 	renderSquare(i) {
 		return (
-			<Square value={this.props.squares[i]} 
+			<Square key={i} value={this.props.squares[i]} 
 			onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 
 	render () {
-		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
+		let rows = [];
+	    let squares = [];
+		for (let r = 0; r < 3; r++) {
+			for (let i = r * 3; i < r*3 + 3; i++) {
+        		squares.push(this.renderSquare(i));
+      		}
+      		rows.push(<div key={r} className="board-row">{squares}</div>);
+      		squares = [];
+    	}
+
+    	return <div>{rows}</div>;
 	}
 }
 
